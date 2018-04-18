5300d19c43064552ded5598b8c830deb394ac1b2
xophio <latheef.razzaq@gmail.com>
2018-04-18 16:26:29 +0500

    home routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/login.service';
+import { RootRoutingModule } from './/root-routing.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { LoginService } from './login/login.service';
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    RootRoutingModule
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
