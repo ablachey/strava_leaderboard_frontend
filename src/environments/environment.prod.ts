48cdc3de63d40d1f7e321b34f0b2bec846c01463
Abdul Latheef <abdullatheef@Abduls-Mac-mini.local>
2018-04-24 03:16:01 +0500

    updated env for callback

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiBase: "https://leaderboard-api.clayapps.com/api/",
-  stravaAuth: "https://www.strava.com/oauth/authorize?client_id=24837&response_type=code&redirect_uri=http://localhost:4200/auth/authorize&approval_prompt=force&scope=public",
+  stravaAuth: "https://www.strava.com/oauth/authorize?client_id=24837&response_type=code&redirect_uri=http://leaderboard.clayapps.com/auth/authorize&approval_prompt=force&scope=public",
   titleBase: 'Leaderboard'
 };
