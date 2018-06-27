0953c1f47e9a59caa281291812c8cbf87f4f3db0
xophio <latheef.razzaq@gmail.com>
2018-06-27 13:40:03 +0500

    loader changed to css

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -4,29 +4,7 @@ import { AlertService } from './alert.service';
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
-  styles: [
-    `
-    .loading-screen {
-      position: absolute;
-      width: 200px;
-      left: -webkit-calc(50% - 100px);
-      left: -moz-calc(50% - 100px);
-      left: calc(50% - 100px);
-      z-index: 1000;
-    }
-    
-    .loading-text {
-      position: relative;
-      font-size: 30px;
-      color: rgba(252, 76, 2, 0.65);
-    }
-    
-    .loading-content {
-      position: relative;
-      margin-top: 100px;
-    }
-    `
-  ]
+  styleUrls: ['./alert.component.css']
 })
 export class AlertComponent implements OnInit {
 
